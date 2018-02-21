5740fea45282b52946f63340798cbe6771f555f4
Bryan Gilomen <gilomen@hotmail.com>
2018-02-21 17:24:33 -0500

    initial load

---
+++ TestApp/TestApp/testform.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestApp
+{
+    public partial class testform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int x = 1;
+            int y = 2;
+            lblOut.Text = "This is a test";
+        }
+    }
+}
